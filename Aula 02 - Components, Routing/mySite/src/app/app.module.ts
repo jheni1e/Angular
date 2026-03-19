b00a7300930b2d8e4405b6258affb5961771f000
Jhenie <highhalma@gmail.com>
2026-03-19 11:03:53 -0300

    project structure fixed

--- Aula 02 - Components, Routing/mySite/src/app/app.module.ts
+++ Aula 02 - Components, Routing/mySite/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavComponent } from './components/nav/nav.component';
-import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './shared/nav/nav.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { LoginPageComponent } from './features/login-page/login-page.component';
+import { MainPageComponent } from './features/main-page/main-page.component';
+import { NotFoundComponent } from './features/not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginPageComponent,
+    MainPageComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
